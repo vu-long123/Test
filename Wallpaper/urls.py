ed7725a75bd7736054e1e7397dcba8990ddf1253
vu-long123 <90613262+vu-long123@users.noreply.github.com>
2021-11-10 17:01:32 +0700

    Add files via upload

---
+++ Wallpaper/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.Home,name="home"),
+    path('results/',views.results,name="results")
+]
